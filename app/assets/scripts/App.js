8804c319fcc0b62686ee592ed0c55be453e098dc
Andrew <niuniup10@gmail.com>
2020-05-01 16:38:33 -0600

    completed lazy loading of images.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,14 +1,29 @@
 import "../styles/styles.css";
+import "lazysizes";
 import MobileMenu from "./modules/MobileMenu";
 import RevealOnScroll from "./modules/RevealOnScroll";
 import StickyHeader from "./modules/StickyHeader";
 
-let stickyHeader = new StickyHeader();
+new StickyHeader();
 new RevealOnScroll(document.querySelectorAll(".feature-item"), 75);
 new RevealOnScroll(document.querySelectorAll(".testimonial"), 60);
+new MobileMenu();
+let modal;
 
-let mobileMenu = new MobileMenu();
+document.querySelectorAll(".open-modal").forEach(el => {
+    el.addEventListener("click", e => {
+        e.preventDefault();
+        if (typeof modal == 'undefined') {
+            import(/* webpackChunkName: 'modal' */ './modules/Modal').then(x => {
+                modal = new x.default()
+                setTimeout(() => modal.openTheModal(), 20);
+            }).catch(() => console.log('problem dected'));
+        } else {
+            modal.openTheModal()
+        }
+    })
+})
 
-if(module.hot) {
+if (module.hot) {
     module.hot.accept()
 }
